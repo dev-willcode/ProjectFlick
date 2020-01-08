1ba955e6229658e606d4d56f16f13ff77b4259f0
JorgeJU99 <59629973+JorgeJU99@users.noreply.github.com>
2020-01-07 20:04:10 -0500

    Añadidas clases Pelicula & sala_cine
    ## commit de prueba

---
+++ FlickLibrary/Entidades/Sala_Cine.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlickLibrary.Entidades
+{
+    class Sala_Cine
+    {
+        private int id;
+        private string sala_nombre;
+        private string salsa_capacidad_maxima;
+
+        public Sala_Cine()
+        {
+        }
+
+        public Sala_Cine(int id, string sala_nombre, string salsa_capacidad_maxima)
+        {
+            this.id = id;
+            this.sala_nombre = sala_nombre;
+            this.salsa_capacidad_maxima = salsa_capacidad_maxima;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string Sala_nombre { get => sala_nombre; set => sala_nombre = value; }
+        public string Salsa_capacidad_maxima { get => salsa_capacidad_maxima; set => salsa_capacidad_maxima = value; }
+    }
+}
