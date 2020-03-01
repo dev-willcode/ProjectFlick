1a2014e114b158f6955e657a38f5c0ad10f0c42a
@dev.will <51866883+dev-willcode@users.noreply.github.com>
2020-03-01 18:12:09 -0500

    Implementación inicial - android

---
+++ AppFlickMovil/AppFlickMovil/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using AppFlickMovil.Models;
+
+namespace AppFlickMovil.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
