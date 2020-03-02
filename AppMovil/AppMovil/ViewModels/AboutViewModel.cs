6b2cbcfb3f84ed3c4774aa7354cf23adef663bf8
@dev.will <51866883+dev-willcode@users.noreply.github.com>
2020-03-02 17:23:09 -0500

    Push de nueva instancia de app

---
+++ AppMovil/AppMovil/ViewModels/AboutViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace AppMovil.ViewModels
+{
+    public class AboutViewModel : BaseViewModel
+    {
+        public ICommand OpenWebCommand { get; }
+    }
+}
