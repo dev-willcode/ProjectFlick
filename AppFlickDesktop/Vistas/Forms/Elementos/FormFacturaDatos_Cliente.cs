8ab7e61e0564a8899a7a1b7ffe1d5f1b324becc4
Will <dev.willct@gmail.com>
2020-03-03 16:50:23 -0500

    Creada estructura para reportes

--- AppFlickDesktop/Vistas/Forms/Elementos/FormFacturaDatos_Cliente.cs
+++ AppFlickDesktop/Vistas/Forms/Elementos/FormFacturaDatos_Cliente.cs
@@ -4,6 +4,7 @@ using System.Windows.Forms;
 using AppFlickCliente.Reports;
 using AppFlickCliente.Reports.DataSets;
 using Controllers;
+using Controllers.Reportes;
 using CrystalDecisions.CrystalReports.Engine;
 using CrystalDecisions.Shared;
 using Entity.Entidades;
@@ -81,57 +82,11 @@ namespace AppFlickCliente.Vistas.Forms.Elementos
         {
             try
             {
-                DSFacturaDatos dsFacturaDatos = new DSFacturaDatos();
-                vistaFactura = PropiedadesGenerales.VFacturaDatosController.GetDatosFactura(id);
-                empresa = PropiedadesGenerales.EmpresaController.Get(1);
-                boletoFactura = PropiedadesGenerales.BoletosFacturaController.getBoletosFactura(id);
-                RPFacturaDatos rPFactura = new RPFacturaDatos();
-
-                dsFacturaDatos.Empresa.AddEmpresaRow
-                    (
-                    empresa.id,
-                    empresa.empresa_razon_social,
-                    empresa.empresa_numero_contribuyente,
-                    empresa.empresa_numero_autorizacion,
-                    empresa.empresa_direccion,
-                    empresa.empresa_telefono,
-                    empresa.empresa_email,
-                    empresa.empresa_website,
-                    empresa.empresa_ambiente,
-                    empresa.empresa_contabilidad
-                    );
-                dsFacturaDatos.Vista_FacturaDatos.AddVista_FacturaDatosRow(
-                    vistaFactura.id,
-                    vistaFactura.cliente_cedula,
-                    vistaFactura.cliente_apellidos,
-                    vistaFactura.cliente_nombres,
-                    vistaFactura.cliente_telefono,
-                    vistaFactura.cliente_email,
-                    vistaFactura.cliente_direccion,
-                    vistaFactura.factura_numero,
-                    vistaFactura.factura_fecha_emision,
-                    vistaFactura.factura_metodo_pago,
-                    vistaFactura.funcion_fecha_evento,
-                    vistaFactura.funcion_precio_boleto,
-                    vistaFactura.sala_nombre,
-                    vistaFactura.horario_inicio,
-                    vistaFactura.idioma_abreviatura,
-                    vistaFactura.pelicula_titulo,
-                    vistaFactura.pelicula_tipo_censura,
-                    vistaFactura.pelicula_duracion
-                );
-                dsFacturaDatos.SP_ObtenerBoletosFactura.AddSP_ObtenerBoletosFacturaRow(
-                    boletoFactura.cantidad,
-                    boletoFactura.asientos,
-                    boletoFactura.concepto,
-                    boletoFactura.precio_unitario,
-                    boletoFactura.importe,
-                    boletoFactura.iva,
-                    boletoFactura.total
-                );
-                rPFactura.SetDataSource(dsFacturaDatos);
-                crystalReportViewer1.ReportSource = rPFactura;
-                enviarEmail(rPFactura);
+                ReporteController reporteController = new ReporteController();
+                if (reporteController.generarReporte(id)) { 
+                    crystalReportViewer1.ReportSource = reporteController.reporte;
+                    enviarEmail(reporteController.reporte);
+                }
             }
             catch (ControllerException ex)
             {
---
+++ Controllers/Reportes/ReporteController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CrystalDecisions.CrystalReports.Engine;
+
+namespace Controllers.Reportes
+{
+    public class ReporteController
+    {
+        public byte[] byteReporte { get; set; }
+        public ReportDocument reporte { get; set; }
+
+        public ReporteController()
+        {
+        }
+
+        /*
+         * Método que genera el reporte de una factura, a partir de la id especificada
+         * genera un el reporte, que es usado para representar el archivo
+         */
+        public bool generarReporte(int idFactura)
+        {
+            return false;
+        }
+        
+    }
+}
