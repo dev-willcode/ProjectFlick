870629299be09cd1bdbd35207ac80f6de2c0a844
JorgeJU99 <59629973+JorgeJU99@users.noreply.github.com>
2020-03-02 23:00:21 -0500

    Proyecto Email

---
+++ DataEmail/Emailer/IEmailSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emailer
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string emailTo, string subject, string message);
+    }
+}
